e2550d9adc2011f43dca140d0b8d8e44b0f3c8ee
Mostafa Waleeed <mostafawaleed206@gmail.com>
2022-12-24 09:47:52 +0200

    📦Code Refactoring: delete `dynamic` and `useMemo`

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -1,15 +1,7 @@
-import dynamic from 'next/dynamic';
 import { Hits, SortBy, Configure } from 'react-instantsearch-hooks-web';
-import { useMemo, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import useMediaQuery from 'hook/useMediaQuery';
-
-const FilterView = dynamic(() => import('components/filter/FilterView'), {
-  ssr: false
-});
-
-const Hit = dynamic(() => import('components/filter/Hit'), {
-  ssr: false
-});
+import { FilterView, Hit } from 'components/filter';
 
 interface Props {
   title: string;
@@ -23,11 +15,6 @@ export default function Search({ title, category, colors, categories }: Props) {
   const [showFilter, setShowFilter] = useState<boolean>(true);
   const matches = useMediaQuery(1024, filter, showFilter, setShowFilter);
 
-  const filterView = useMemo(
-    () => <FilterView categories={categories} colors={colors} />,
-    []
-  );
-
   return (
     <div className="container region-md">
       <div>
@@ -35,7 +22,9 @@ export default function Search({ title, category, colors, categories }: Props) {
         <div className="region-md">
           <div className="flex lg:justify-between gap-12" ref={filter}>
             <Configure filters={category} />
-            <div className={matches ? '' : 'sr-only'}>{filterView}</div>
+            <div className={matches ? '' : 'sr-only'}>
+              <FilterView categories={categories} colors={colors} />
+            </div>
             <div className="w-full">
               <div className="flex justify-end items-center">
                 <div className="block">
