f59ca34ab333d46317d79cef10de09322a8ca082
Mostafa Waleed <87701479+mostafa-mw@users.noreply.github.com>
2023-02-19 17:00:21 +0200

    Delete unused `meta`

--- components/common/Container/Container.tsx
+++ components/common/Container/Container.tsx
@@ -23,18 +23,9 @@ export default function Container(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
-        {router.asPath === '/' ? (
-          <meta name="google-site-verification" content="" />
-        ) : null}
         <title>{prop.title}</title>
         <meta content={prop.description} name="description" />
         <link rel="canonical" href={`https://www..${router.asPath}`} />
-        <meta name="robots" content="follow, index" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="" />
-        <meta name="twitter:title" content={prop.title} />
-        <meta name="twitter:description" content={prop.description} />
-        <meta name="twitter:image" content={prop.image} />
       </Head>
       <Header />
       <main id="main-content" tabIndex={-1}>
